0f6e711a89cae659d0678ca2e344f1cf7029f250
Lakshmi1621 <koribillilaxmi39@gmail.com>
2025-02-20 23:38:02 +0530

    Add files via upload

---
+++ src/main/java/com/app/movie/controller/HomepageController.java
@@ -0,0 +1,40 @@
+package com.app.movie.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.app.movie.entity.MovieEntity;
+import com.app.movie.service.MovieService;
+
+@Controller
+@RequestMapping("/movieapp")
+public class HomepageController {
+	
+	@Autowired
+    private MovieService movieService;
+    
+    @GetMapping("/home")
+    public String home(Model model) {
+    	    
+    	List<MovieEntity> movieList = movieService.getMovies();
+        
+    	model.addAttribute("movieList", movieList);
+    	
+        return "homepage";
+    }
+
+    
+    @GetMapping("/movie-api")
+    @ResponseBody
+    public List<MovieEntity> getMovies() {
+    	List<MovieEntity> movieApiList = movieService.getMovies();
+    	
+        return movieApiList;
+    }
+}
